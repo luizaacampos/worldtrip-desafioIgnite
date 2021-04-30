0b24ecddd518fdc31f255da8b169b9a0a83f2012
Luiza Campos <luiza.almcampos@gmail.com>
2021-04-30 09:36:39 -0300

    projeto base

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | worldtrip</title>
+      </Head>
+      <h1>Worldtrip</h1>
+    </>
+     
+  )
+}
